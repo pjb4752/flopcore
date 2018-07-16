842cfec316a91ca7d43c228479fab7fc6b7e6c5f
pjb4752 <pjb4752@gmail.com>
2018-07-16 19:35:52 -0400

    add operator wrapper functions

--- core/common.lua
+++ core/common.lua
@@ -7,4 +7,24 @@ end
 Common.num_to_str = Common.to_str
 Common.bool_to_str = Common.to_str
 
+Common.plus = function(l, r)
+  return l + r
+end
+
+Common.minus = function(l, r)
+  return l - r
+end
+
+Common.mult = function(l, r)
+  return l * r
+end
+
+Common.div = function(l, r)
+  return l / r
+end
+
+Common.mod = function(l, r)
+  return l % r
+end
+
 return Common
